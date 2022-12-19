b7eba33549adb72d514e5a4e2b0ddbcc6b38084c
Isiah Fletcher <Isiahfletcher7@gmail.com>
2022-12-19 14:50:12 -0700

    init with styled-components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,27 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const meta = {
+    title: "Shelbz Citrine",
+    description: "A barber extraordinaire",
+    // image: "TODO",
+  };
+
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="robots" content="follow, index" />
+        <meta name="description" content={meta.description} />
+        <meta property="og:site_name" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        {/* TODO: Add image */}
+        {/* <meta property="og:image" content={meta.image} /> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
