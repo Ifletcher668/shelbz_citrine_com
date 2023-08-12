a54f62e99d0e81d4c4e433e3e2e6784f46bce905
Isiah Fletcher <Isiahfletcher7@gmail.com>
2023-08-11 18:28:16 -0600

    Eager load hero image

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,9 +38,7 @@ const Home = (props: Props) => {
             alt={''}
             width={1332}
             height={666}
-            loading="lazy"
-            placeholder="blur"
-            blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiB2aWV3Qm94PSIwIDAgMTAwIDEwMCIgdmVyc2lvbj0iMS4xIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxwYXRoIGQ9Ik0wIDBoMTAwdjEwMEgwVjB6IiBmaWxsPSIjZmZmIi8+PC9zdmc+"
+            loading="eager"
           />
 
           <Row rowSpacing={40}>
