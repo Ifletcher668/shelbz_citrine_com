521c47bd28afd78658f114c8039f12c4cbb952a9
Isiah Fletcher <Isiahfletcher7@gmail.com>
2025-01-04 19:33:17 -0800

    Add fallback if grid data is undefined

--- components/PictureGrid/PictureGrid.tsx
+++ components/PictureGrid/PictureGrid.tsx
@@ -12,10 +12,10 @@ import Pagination from '../Pagination';
 import Picture from './Picture';
 
 type PictureGridProps = {
-  data: ContentfulImage[];
+  data?: ContentfulImage[];
 };
 
-const PictureGrid = ({ data }: PictureGridProps) => {
+const PictureGrid = ({ data = [] }: PictureGridProps) => {
   const gridRef = useRef<HTMLDivElement | null>(null);
   const [currentPage, setCurrentPage] = useState(1);
 
